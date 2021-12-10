fc50b4e43f3c408c6d20158940ed9bc5acfec6c7
iniyavandesign <foriniyavan@gmail.com>
2021-12-10 20:10:34 +0530

    Update

--- js/googletag.js
+++ js/googletag.js
@@ -1,9 +1,7 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q375GEW9HJ"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-Q375GEW9HJ');
-</script>
