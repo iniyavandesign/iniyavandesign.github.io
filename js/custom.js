d3604df129c6cd7e51830d786c56b1baf9c30815
iniyavan <87469256+iniyavandesign@users.noreply.github.com>
2023-04-12 20:01:39 +0530

    update

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+  });
+  
+  sr.reveal(`.profile__border`);
+  sr.reveal(`.header_section`, { delay: 500 });
+  sr.reveal(`.work`, { delay: 600 });
+  sr.reveal(`.work_title`, { delay: 700 });
+  sr.reveal(`.picture`, { interval: 100, delay: 700 });
+  sr.reveal(`.hire`, { delay: 800 });
+  sr.reveal(`.filters__content`, { delay: 900 });
+  sr.reveal(`.filters`, { delay: 1000 });
