b4dc493a052fd768d00e6583b0b84951f293a17a
iniyavan <87469256+iniyavandesign@users.noreply.github.com>
2023-04-15 17:33:46 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -5,12 +5,7 @@ const sr = ScrollReveal({
     duration: 2500,
     delay: 400,
   });
-  
+  `q`
   sr.reveal(`.profile__border`);
-  sr.reveal(`.header_section`, { delay: 500 });
-  sr.reveal(`.work`, { delay: 600 });
-  sr.reveal(`.work_title`, { delay: 700 });
-  sr.reveal(`.picture`, { delay: 700 });
-  sr.reveal(`.hire`, { delay: 300 });
-  sr.reveal(`.filters__content`, { delay: 900 });
-  sr.reveal(`.filters`, { delay: 1000 });
+  sr.reveal(`.header_section`, { delay: 500 });````` htyviuoi[poho;]`````
+  sr.reveal(`.filters`, { delay: 1000 });N
